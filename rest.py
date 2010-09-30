a1195c7d1419006b8fb75dc8d4cd75e31af19f5d
Aleksandar Gabrovski <gabrovski@dartmouth.edu>
2010-09-30 21:31:11 +0200

    found an easy way to query bing and get links to restaurant profiles in opentable. need to make query more accurate since bing sux. soon will be ready to scrape. will resuse concurrent scraper built for smth else

--- rest.py
+++ rest.py
@@ -1,5 +1,10 @@
 #####
 import re
+import urllib2
+
+BINGBASE = 'http://www.bing.com/search?q='
+BINGEND = '&go=&form=QBRE&filt=all'
+WEBSITE = 'opentable.com'
 
 def getRest(inp):
     f = open(inp, 'r')
@@ -7,18 +12,39 @@ def getRest(inp):
     f.close()
 
     for x in xrange(len(t)):
-        t[x] = re.findall('\"(.*?)\"', t[x], re.U)
+        t[x] = re.findall('\"(.*?)\"', t[x])
         #print t[x]
     return t
 
 def filterNames(l):
     res = []
     for x in l:
-        if x[0].rfind('\\x') == -1:
+        if str(x[0]).rfind('\\x') == -1:
             res.insert(0, x)
     #print res
     return res
 
+#idetifief opentable.com as the website to use. will use google to get links to the restaurants. google api is not very good, important info often is not present in the available database (missing pages that can be found using the regular search engine). that's why will use google search engine in general with modification of the url so i get the real deal. 
+#Correction:Use Bing
+def lookUp(query, website, base, end):
+    query = re.sub('[ \t]', '+', query)
+    query = re.sub('[,\.\']', '', query)
+    print query
+    f = urllib2.urlopen(base+query+'+'+website+end)
+    txt = f.read()
+    f.close()
+    #print txt
+    return txt
+
+def getLink(txt, s):
+    m = re.search(s, txt)
+    if m:
+        return m.group(1)
+    else:
+        return None
+
 if __name__ == '__main__':
     a = getRest('restaurants.csv')
-    print filterNames(a)
+    a = filterNames(a)
+    r = lookUp(a[2][0]+a[2][len(a[2])-1],WEBSITE,BINGBASE, BINGEND)
+    print getLink(r, '(http://www.opentable.*?)\"')
